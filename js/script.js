c97e6ccd3e802296df5beec91b3144d04cde62cf
nicokant94 <niccolocantu94@hotmail.it>
2014-11-21 17:56:10 +0100

    app

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+user="";
 window.onload=function(){
-    //user=prompt();
-    user="test";
-    //console.log('start');
+
+    if(user!=""){
+        $('#splashscreen').addClass('show');
+        $('#splashscreen').removeClass('hidden');
+    }else{
+        $('#projects-list-view').addClass('show');
+            $('#projects-list-view').removeClass('hidden');
+    }
+
     fb = new Firebase("http://ffworkgroup.firebaseio.com");
-    //console.log(fb);
+    $('#user-input').submit(function(){
+         if($('#user').val()!=""){
+            user=$('#user').val();
+            
+            $('#splashscreen').addClass('hidden');
+            $('#splashscreen').removeClass('show');
+            
+            $('#projects-list-view').addClass('show');
+            $('#projects-list-view').removeClass('hidden');
+         }   
+    });
+
+    //user=prompt();
+    //console.log('start');
+    
+    console.log(fb);
     currentId=null;
     //setInterval(function(){console.log(currentId);},2000);
     
@@ -15,6 +37,28 @@ window.onload=function(){
         writeProjects(projects);
         if(currentId!=null)
             writeMessages(projects[currentId].messages);
+            
+        $(document).on('click','.back',function(){
+            $('#conversation-view').addClass('hidden');
+            $('#conversation-view').removeClass('show');
+            
+            $('#new-project').addClass('hidden');
+            $('#new-project').removeClass('show');
+            
+            $('#projects-list-view').addClass('show');
+            $('#projects-list-view').removeClass('hidden');
+        });
+        
+        
+        $(document).on('click','.new',function(){
+            $('#name').val("");
+            $('#project-creator').val("");
+            $('#new-project').addClass('show');
+            $('#new-project').removeClass('hidden');
+            
+            $('#projects-list-view').addClass('hidden');
+            $('#projects-list-view').removeClass('show');
+        });
         
         
         $(document).on('click','.project-link',function(){
@@ -24,6 +68,12 @@ window.onload=function(){
             currentId=id;
             $('#message-title').append(projects[id].name);
             writeMessages(projects[id].messages);
+            
+            $('#conversation-view').addClass('show');
+            $('#conversation-view').removeClass('hidden');
+            
+            $('#projects-list-view').addClass('hidden');
+            $('#projects-list-view').removeClass('show');
         });
         
         $('#mex-form').submit(function(){
@@ -34,8 +84,9 @@ window.onload=function(){
         
         $('#pr-form').submit(function(){
             console.log('project sent');
-            name=$('#project-creator').val();
-            fb.push({'name':name,'messages':0});
+            description=$('#project-creator').val();
+            name=$('#name').val();
+            fb.push({'name':name,'description':description,'messages':0});
         });
         
     });
@@ -44,13 +95,31 @@ window.onload=function(){
 function writeProjects(projects){
     for(id in projects){
         project=projects[id];
-        $('#projects').prepend('<li><a class="project-link" href="#" data-id="'+id+'">'+project.name+'</a></li>');
+        count=0;
+        if(project.messages!=0){
+            for(mex in project.messages){
+                count++;
+            }
+        }
+        $('#projects').prepend('<li class="list-group-item"><a class="project-link" href="#" data-id="'+id+'"><div class="pr-desc"><h4 class="list-group-item-heading">'+project.name+'</h4><p class="list-group-item-text">'+project.description+'</p></div><div class="span-badge"><span class="badge">'+count+'</span></div></a></li>');
+        
     }
 }
 
 function writeMessages(messages){
+    counter=0;
     for(id in messages){
         message=messages[id];
-        $('#message-list').prepend('<li><div class="panel panel-default"><div class="panel-heading">'+message.auth+'says:</div><div class="panel-body">'+message.text+'</div></div></li>');
+        
+        switch(counter){
+            case 0:$('#message-list').append('<div class="panel panel-success"><div class="panel-heading">'+message.auth+' says:</div><div class="panel-body">'+message.text+'</div></div>');break;
+            
+            case 1:$('#message-list').append('<div class="panel panel-info"><div class="panel-heading">'+message.auth+' says:</div><div class="panel-body">'+message.text+'</div></div>');break;
+            
+            case 2:$('#message-list').append('<div class="panel panel-warning"><div class="panel-heading">'+message.auth+' says:</div><div class="panel-body">'+message.text+'</div></div>');break;
+            
+            case 3:$('#message-list').append('<div class="panel panel-danger"><div class="panel-heading">'+message.auth+' says:</div><div class="panel-body">'+message.text+'</div></div>');break;
+        }
+        counter=(counter+1)%4;
     }
 }
